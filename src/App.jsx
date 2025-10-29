dd42e4ea9fcc4bddd18e3b86cd27b3879c943f40
xubihang <xubihang@gmail.com>
2025-10-29 15:39:52 +0800

    Create basic React hello world app

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>你好，React！</h1>
+      <p>这是一个使用 React 构建的基础 Hello World 示例。</p>
+    </div>
+  );
+}
+
+export default App;
